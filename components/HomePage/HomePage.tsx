12f4ede3d1a63b2edd75cd68a9274efe83f4c4b1
Azeroji <daliladimi33@gmail.com>
2023-05-20 17:49:23 +0100

    done

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const HomePage = (props: Props) => {
   return (
-    <div className='flex justify-center py-[20%] md:py-[10%]'>
+    <div className='flex justify-center pt-[20%] pb-[35%] md:py-[10%]'>
       <div className='w-[85%] md:w-[60%] flex flex-col gap-y-[20px]'>
         <p className='text-TXT font-FiraCode leading-none'>Hello, my name is</p>
         <p className='text-white font-Poppins font-semibold text-[32px] md:text-[60px] leading-none'>Dalil ADIMI<span className='text-TXT'>.</span></p>
