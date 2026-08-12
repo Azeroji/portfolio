c9d8263822dbce18dc55d8c3bd07bb250924b331
Azeroji <115744926+Azeroji@users.noreply.github.com>
2026-08-12 18:51:07 +0100

    Focus portfolio on bug bounty hunting
    Remove web development positioning

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -9,9 +9,8 @@ const HomePage = (props: Props) => {
       <div className='w-[85%] md:w-[60%] flex flex-col gap-y-[20px]'>
         <p className='text-TXT font-FiraCode leading-none'>Hello, my name is</p>
         <p className='text-white font-Poppins font-semibold text-[32px] md:text-[60px] leading-none'>Dalil ADIMI<span className='text-TXT'>.</span></p>
-        <p className='text-GRAY font-Poppins font-semibold text-[32px] md:text-[60px] leading-none'>I am a <span className='text-TXT'>Full Stack Web Developer</span>.</p>
-        <p className='text-GRAY font-Poppins font-semibold text-[22px] md:text-[30px] leading-none'>and a <span className='text-TXT'>Bug Bounty Hunter @ YesWeHack</span>.</p>
-        <p className='text-GRAY font-FiraCode w-[60%]'>I've built and conceived multiple web projects both front-only and full stack applications, while responsibly researching the security of real-world web applications.</p>
+        <p className='text-GRAY font-Poppins font-semibold text-[32px] md:text-[60px] leading-none'>I am a <span className='text-TXT'>Bug Bounty Hunter @ YesWeHack</span>.</p>
+        <p className='text-GRAY font-FiraCode w-[60%]'>I responsibly research the security of real-world applications and disclose vulnerabilities through YesWeHack.</p>
         <Link href="#Contact" className='text-center hover:text-TXT font-FiraCode border border-TXT px-[15px] py-[8px] hover:bg-BG bg-TXT text-BG rounded-[3px] w-[150px] font-medium mt-[5%]'>Contact me</Link>
       </div>
     </div>
